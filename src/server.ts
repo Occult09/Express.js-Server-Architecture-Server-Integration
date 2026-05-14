efff34027d688673c2c0eeadf663f979cdefe233
Occult09 <maheermusfiq221@gmail.com>
2026-05-14 15:39:30 +0600

    Set up Postgress Neon serverless cloud

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,35 @@
-import express from "express"
-const app = express();
+import express, { type Application, type Request, type Response } from "express"
+import {Pool} from 'pg'
+const app: Application = express();
 const port = 5000;
 
-app.get('/user', (req, res) => {
-  res.send('Hello World!!!')
+//middleware
+app.use(express.json()); 
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_ksmiz6rq9Iep@ep-wandering-waterfall-ap58op8f.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
+})
+
+app.get('/user', (req: Request, res: Response) => {
+//   res.send('Hello World!!!')
+    res.status(200).json({
+        "message":"Express Server",
+        "author":"Maheer",
+    })
+})
+
+app.post('/', async(req: Request, res: Response)=>{
+    // console.log(req.body);
+    const {name,course,password} = req.body;
+    res.status(201).json({
+        message: 'Created',
+        data: {
+            name,course
+        }
+    })
 })
 
 app.listen(port, () => {
